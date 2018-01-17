a9bd2ddf681a43c94f8dc30e8f10f6abbcab3463
Geurt Lagemaat <geurtlagemaat@gmail.com>
2018-01-17 14:17:51 +0100

    dummy settings

--- __init__.py
+++ __init__.py
@@ -1,1 +1,10 @@
-__author__ = 'geurt'
+# Meta.
+
+__title__ = 'fitbit'
+__author__ = 'Geurt Lagemaat'
+__author_email__ = 'geurtlagemaat@gmail.com'
+__copyright__ = 'Copyright 2016-2018 Geurt Lagemaat'
+__license__ = 'Apache 2.0'
+
+__version__ = '0.3.0'
+__release__ = '0.3.0'
